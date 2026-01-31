189255be43c0d503d7f672cccd186d629c4b2ad5
Hannes Väster <hannesv@tlu.ee>
2026-01-31 16:54:21 +0200

    added postgres, docker and health check

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,7 +2,6 @@ import os
 from sqlalchemy import create_engine, text
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 def check_db_connection() -> bool:
